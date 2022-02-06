a3acd27f1beffdc09bb464c56bd9b5917b79ed83
Tajuddincse <taazctz@gmai.com>
2022-02-06 20:42:28 +0530

    UI fixes

--- src/app/availability/availability.component.ts
+++ src/app/availability/availability.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./availability.component.css']
 })
 export class AVAILABILITYComponent implements OnInit {
-
+  slotList: any = [];
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  setValue(slot: string){
+    if(this.slotList.indexOf(slot) == -1){
+      this.slotList.push(slot);
+    } else if(this.slotList.indexOf(slot) > -1){
+      const index = this.slotList.indexOf(slot);
+      this.slotList.splice(index, 1);
+    }
+    console.log(JSON.stringify(this.slotList));
+  }
+  checkNotInArray(slot: string){
+    if(this.slotList.indexOf(slot) > -1){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
